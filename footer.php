759ce576ced8ac47e74947583660b65fd63c68ee
BillMukholi <work.billmukholi@gmail.com>
2024-11-06 20:47:03 +0300

    Updated Footer

--- footer.php
+++ footer.php
@@ -15,14 +15,39 @@
 		<div class="footer-top-conainer">
 			<div class="footer-top">
 				<div class="footer-top-left">
-				<?php
-					wp_nav_menu( array(
-						'theme_location' => 'footer-about',
-						'menu_class'     => 'footer-menu', // Add your own class for styling
-						'container'      => 'menu',               // Container element, e.g., 'div', 'nav'
-						'container_class'=> 'footer-container', // Class for the container
-					) );
-					?>
+					<div class="footer-item">
+						<p class="footer-item-header">About</p>
+						<?php
+						wp_nav_menu( array(
+							'theme_location' => 'footer-about',
+							'menu_class'     => 'footer-menu', // Add your own class for styling
+							'container'      => 'menu',               // Container element, e.g., 'div', 'nav'
+							'container_class'=> 'footer-container', // Class for the container
+						) );
+						?>
+					</div>
+					<div class="footer-item">
+						<p class="footer-item-header">Account</p>
+						<?php
+						wp_nav_menu( array(
+							'theme_location' => 'footer-account',
+							'menu_class'     => 'footer-menu', // Add your own class for styling
+							'container'      => 'menu',               // Container element, e.g., 'div', 'nav'
+							'container_class'=> 'footer-container', // Class for the container
+						) );
+						?>
+					</div>
+					<div class="footer-item">
+						<p class="footer-item-header">Support</p>
+						<?php
+						wp_nav_menu( array(
+							'theme_location' => 'footer-support',
+							'menu_class'     => 'footer-menu', // Add your own class for styling
+							'container'      => 'menu',               // Container element, e.g., 'div', 'nav'
+							'container_class'=> 'footer-container', // Class for the container
+						) );
+						?>
+					</div>
 				</div>
 				<div class="footer-top-right"></div>
 			</div>
