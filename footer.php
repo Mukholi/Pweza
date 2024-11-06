fdf195cc5bf769292f04622c2288abc1696fd8b8
BillMukholi <work.billmukholi@gmail.com>
2024-11-06 22:11:59 +0300

    Updated Footer

--- footer.php
+++ footer.php
@@ -49,11 +49,51 @@
 						?>
 					</div>
 				</div>
-				<div class="footer-top-right"></div>
+				<div class="footer-top-right">
+					<div class="footer-top-search">
+						<?php get_search_form(); ?>
+					</div>
+					<div class="footer-top-links">
+						<?php
+						// Define your theme locations.
+						$first_menu_location = 'footer-newsletter';
+						$second_menu_location = 'footer-terms&conditions';
+
+						// Initialize variables to store the last links.
+						$first_last_link = '';
+						$second_last_link = '';
+
+						// Get the last link from the first menu location.
+						if (($locations = get_nav_menu_locations()) && isset($locations[$first_menu_location])) {
+							$first_menu = wp_get_nav_menu_object($locations[$first_menu_location]);
+							$first_menu_items = wp_get_nav_menu_items($first_menu->term_id);
+
+							if (!empty($first_menu_items)) {
+								$first_last_item = end($first_menu_items);
+								$first_last_link = '<a href="' . esc_url($first_last_item->url) . '">' . esc_html($first_last_item->title) . '</a>';
+							}
+						}
+
+						// Get the last link from the second menu location.
+						if (isset($locations[$second_menu_location])) {
+							$second_menu = wp_get_nav_menu_object($locations[$second_menu_location]);
+							$second_menu_items = wp_get_nav_menu_items($second_menu->term_id);
+
+							if (!empty($second_menu_items)) {
+								$second_last_item = end($second_menu_items);
+								$second_last_link = '<a href="' . esc_url($second_last_item->url) . '">' . esc_html($second_last_item->title) . '</a>';
+							}
+						}
+						?>
+						<img class="footer-top-link-image" src="<?php echo get_template_directory_uri().'/image/Pweza.png' ?>">
+						<p class="footer-top-link-text">signup for our <?php echo $first_last_link; ?> or view the <?php echo $second_last_link; ?>.</p>
+
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="site-info">
-			<p>Footer</p>
+			<p>site info</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
